2cf2feedd813e7d7ae0d1109bf0a2f56969ef1fe
Sergiu.Cretu <32588214+shyene27@users.noreply.github.com>
2018-02-23 02:03:26 +0200

    Update Exercise7.java

--- src/md/tekwill/jf6/homework/ex7/Cart.java
+++ src/md/tekwill/jf6/homework/ex7/Cart.java
@@ -2,20 +2,22 @@ package md.tekwill.jf6.homework.ex7;
 
 import java.util.ArrayList;
 import java.util.List;
+import md.tekwill.jf6.homework.ex7.Item;
 
 // Uncomment the code then implement the missing parts.
 public class Cart {
-//    List<Item> items = new ArrayList<>();
-//
-//    public void addItem(Item item) {
-//        items.add(item);
-//    }
-//    /* Sum up all items prices */
-//    public double getTotalPrice() {
-//        for (Item item: items) {
-//            // ...
-//        }
-//
-//        return total;
-//    }
+    List<Item> items = new ArrayList<>();
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
+    /* Sum up all items prices */
+    double total;
+    public double getTotalPrice() {
+        for (Item item: items) {
+          total+=Item.getPrice(item.price);
+        }
+
+        return total;
+    }
 }
